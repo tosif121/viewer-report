7dffeb88e7ed419dd882fa4c2078fc5d237c46a3
tossi <tosifraza8867@gmail.com>
2024-03-31 13:12:50 +0530

    table fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,91 +134,41 @@ ${tableData?.mlcdrName?.compony}`
 MD (Radio-Diagnosis)
 ${tableData?.drName?.compony}`;
 
-  const table = ` <table className="text-dark mb-3 min-w-full whitespace-nowrap border text-center text-sm font-light">
-<thead className="border-b font-medium">
-  <tr>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Patient ID
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Patient Name
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Date
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Study
-    </th>
+  const table = `
+  <table style="border-collapse: collapse; display: inline-table; border: 0px solid black;">
+    <thead>
+    <tr>
+    <th style="border-right: 0.3px solid black; padding: 4px;">Patient ID</th>
+    <th style="border-right: 0.3px solid black; padding: 4px;">Patient Name</th>
+    <th style="border-right: 0.3px solid black; padding: 4px;">Date</th>
+    <th style="border-right: 0.3px solid black; padding: 4px;">Study</th>
   </tr>
-</thead>
-<tbody>
-  <tr className="border-b font-medium">
-    <td className="border-r">${tableData?.patientID}</td>
-    <td className="border-r">${tableData?.name}</td>
-    <td className="border-r">${formattedDate}</td>
-    <td className="border-r">${tableData?.study}</td>
-  </tr>
-</tbody>
-<thead className="border-b font-medium">
-  <tr>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Gender
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Modality
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Age
-    </th>
-    <th
-      scope="col"
-      className="border-r"
-    >
-      Ref Doctor
-    </th>
-  </tr>
-</thead>
-<tbody>
-  <tr className="border-b font-medium">
-    <td className="border-r">${tableData?.PatientSex}</td>
-    <td className="border-r">${tableData?.modality}</td>
-    <td className="border-r">${tableData?.PatientAge}</td>
-    <td className="border-r">${tableData?.ReferringPhysicianName}</td>
-  </tr>
-</tbody>
-</table>`;
-
-  const urlToBase64 = async (url) => {
-    const response = await fetch(url);
-    const blob = await response.blob();
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  };
+    </thead>
+    <tbody>
+      <tr>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.patientID}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.name}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${formattedDate}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.study}</td>
+      </tr>
+    </tbody>
+    <thead>
+      <tr>
+        <th style="border-right: 0.3px solid black; padding: 4px;" scope="col">Gender</th>
+        <th style="border-right: 0.3px solid black; padding: 4px;" scope="col">Modality</th>
+        <th style="border-right: 0.3px solid black; padding: 4px;" scope="col">Age</th>
+        <th style="border-right: 0.3px solid black; padding: 4px;" scope="col">Ref Doctor</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.PatientSex}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.modality}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.PatientAge}</td>
+        <td style="border-right: 0.3px solid black; padding: 4px;">${tableData?.ReferringPhysicianName}</td>
+      </tr>
+    </tbody>
+  </table>`;
 
   const handleDownloadDocx = async () => {
     const fullContent =
@@ -275,52 +225,52 @@ ${tableData?.drName?.compony}`;
       <table className="min-w-full border text-center text-sm font-light text-dark mb-3">
         <thead className="border-b font-medium">
           <tr>
-            <th scope="col" className="border-r">
+            <th scope="col" className=" px-4 py-2 border-r">
               Patient ID
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className=" px-4 py-2 border-r">
               Patient Name
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className=" px-4 py-2 border-r">
               Date
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className=" px-4 py-2 border-r">
               Study
             </th>
             {/* <th
               scope="col"
-              className="border-r"
+              className=" px-4 py-2 border-r"
             >
               Ref Doctor
             </th> */}
           </tr>
         </thead>
         <tbody>
-          <tr className="border-b font-medium">
-            <td className="border-r">{tableData?.patientID}</td>
-            <td className="border-r">{tableData?.name}</td>
-            <td className="border-r">{formattedDate}</td>
-            <td className="border-r">{tableData?.study}</td>
-            {/* <td className="border-r">{tableData.ReferringPhysicianName}</td> */}
+          <tr className=" px-4 py-2 border-b font-medium">
+            <td className=" px-4 py-2 border-r">{tableData?.patientID}</td>
+            <td className=" px-4 py-2 border-r">{tableData?.name}</td>
+            <td className=" px-4 py-2 border-r">{formattedDate}</td>
+            <td className=" px-4 py-2 border-r">{tableData?.study}</td>
+            {/* <td className=" px-4 py-2 border-r">{tableData.ReferringPhysicianName}</td> */}
           </tr>
         </tbody>
         <thead className="border-b font-medium">
           <tr>
-            <th scope="col" className="border-r">
+            <th scope="col" className="px-4 py-2 border-r">
               Gender
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className="px-4 py-2 border-r">
               Modality
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className="px-4 py-2 border-r">
               Age
             </th>
-            <th scope="col" className="border-r">
+            <th scope="col" className="px-4 py-2 border-r">
               Ref Doctor
             </th>
             {/* <th
               scope="col"
-              className="border-r"
+              className="px-4 py-2 border-r"
             >
               Ref Doctor
             </th> */}
@@ -328,11 +278,11 @@ ${tableData?.drName?.compony}`;
         </thead>
         <tbody>
           <tr className="border-b font-medium">
-            <td className="border-r">{tableData?.PatientSex}</td>
-            <td className="border-r">{tableData?.modality}</td>
-            <td className="border-r">{tableData?.PatientAge}</td>
-            <td className="border-r">{tableData?.ReferringPhysicianName}</td>
-            {/* <td className="border-r">{tableData.ReferringPhysicianName}</td> */}
+            <td className="px-4 py-2 border-r">{tableData?.PatientSex}</td>
+            <td className="px-4 py-2 border-r">{tableData?.modality}</td>
+            <td className="px-4 py-2 border-r">{tableData?.PatientAge}</td>
+            <td className="px-4 py-2 border-r">{tableData?.ReferringPhysicianName}</td>
+            {/* <td className="px-4 py-2 border-r">{tableData.ReferringPhysicianName}</td> */}
           </tr>
         </tbody>
       </table>
